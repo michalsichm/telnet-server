ddc694650d3cd07887fb525f4d5fd1d3a236e126
Michal <michalsichm@gmail.com>
2024-10-11 14:17:46 +0200

    test

--- app/src/main/java/utb/fai/App.java
+++ app/src/main/java/utb/fai/App.java
@@ -1,16 +1,27 @@
 package utb.fai;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class App {
 
-    public static void main(String[] args) {
-        int port = 12345;
-        int max_threads = 10;    
+    public static void main(String[] args) throws IOException {
+        int port = Integer.parseInt(args[0]);
+        int max_threads = Integer.parseInt(args[1]);    
 
         // Implement input parameter processing
         
         // Implementation of the main server loop
-        ServerSocket serverSocket;
+        ExecutorService executor = Executors.newFixedThreadPool(max_threads);
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("server started");
+        int clients = 0;
+        while (true) {
+            if (clients == max_threads) break;
+            ClientThread client = new ClientThread(serverSocket.accept());
+            executor.execute(client);
+        }
     }
 }
--- app/src/main/java/utb/fai/ClientThread.java
+++ app/src/main/java/utb/fai/ClientThread.java
@@ -6,6 +6,8 @@ import java.net.*;
 public class ClientThread extends Thread {
 
     private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
 
     public ClientThread(Socket clientSocket) {
         this.clientSocket = clientSocket;
@@ -14,5 +16,25 @@ public class ClientThread extends Thread {
     @Override
     public void run() {
         // Implementation of processing incoming communication from the telnet client
+        try {
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                if (".".equals(inputLine)) {
+                    out.println("bye");
+                    break;
+                }
+                out.println(inputLine);
+                // System.out.println(inputLine);
+            }
+
+            in.close();
+            out.close();
+            clientSocket.close();
+
+        } catch (IOException e) {
+        }
+
     }
 }
